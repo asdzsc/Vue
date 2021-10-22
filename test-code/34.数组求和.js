32ea6eb3fe1016642b5e22e85d4050deb0f234ee
asdzsc <710972821@qq.com>
2021-10-22 15:12:19 +0800

    数组indexOf和includes的区别

--- test-code/34.数组求和.js
+++ test-code/34.数组求和.js
@@ -4,3 +4,8 @@ let arr2 = ['张三', '李四', '王五', '找六', '张三']
 console.log(arr2.reduce((a, b) => a + b)) // => 张三李四王五找六张三
 let arr5 = [1, 2, 3, 4]
 console.log(arr5.reduce((a, b) => a + b)) // => 10
+
+const arr = [1, 2, 'a']
+
+console.log(arr.indexOf('a')) // -1  indexOf找不到-1 找到返回对应的下标
+console.log(arr.includes(3)) // false includes找不到返回 false 找到返回 true
